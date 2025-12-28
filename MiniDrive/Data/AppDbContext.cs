fb4712545b7b5ca056f8d8b067d646a9b1fbd7ba
LeandroMCarv <leandroocarvalhoo9@gmail.com>
2025-12-28 10:58:11 -0300

    init: initial commit

---
+++ MiniDrive/Data/AppDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MiniDrive.Models;
+using System.Collections.Generic;
+
+namespace MiniDriveVideo.Data;
+
+public class AppDbContext : DbContext
+{
+    public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+    {
+
+    }
+
+    public DbSet<ArquivoModel> Arquivos { get; set; }
+}
